67126a1591c91f24df26e224f65e9f6d9060b824
Eren Gölge <egolge@coqui.ai>
2022-05-03 14:09:56 +0200

    Add missing decorator

--- TTS/tts/models/vits.py
+++ TTS/tts/models/vits.py
@@ -187,7 +187,7 @@ def wav_to_mel(y, n_fft, num_mels, sample_rate, hop_length, win_length, fmin, fm
 # CONFIGS
 #############################
 
-
+@dataclass
 class VitsAudioConfig(Coqpit):
     sample_rate: int = 22050
     win_length: int = 1024
